730a7540f4cf4e8cb7138785b01393d5ed4ef416
ribhav1 <70613150+ribhav1@users.noreply.github.com>
2021-09-26 20:46:40 -0400

    Add files via upload

---
+++ data_frame.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = [1, 2, 3, 4, 5]
+df = pd.DataFrame(data)
+
+print(df)
---
+++ plot.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import plotly_express as px
+
+df = pd.read_csv('csv files\line_chart.csv')
+
+fig = px.line(df, x="Year", y="Per capita income", color="Country", title="Per capita income")
+
+fig.show()
